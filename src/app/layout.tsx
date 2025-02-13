0729e25005f4941e37fc357da9e41130e167d153
vinney cavallo <vcavallo@gmail.com>
2025-02-13 16:36:55 -0500

    Update matomo tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -153,12 +153,13 @@ export default function RootLayout({
         {`
           var _paq = window._paq = window._paq || [];
           /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["setDoNotTrack", true]);
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
             var u="//analytics.vaporware.network/";
             _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
+            _paq.push(['setSiteId', '3']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
           })();
